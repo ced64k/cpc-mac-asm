13bf6d2989fa3c0fc63d9019640ee569ddb2ac85
Cedric Simon <ced@64k.be>
2014-02-14 20:05:09 +0100

    no message

--- asm/example.asm
+++ asm/example.asm
@@ -1,10 +1,13 @@
-	ORG   40000   ; Implantation en 40000
-	LD    A,1     ; A chargé avec le mode
-	CALL  #BC0E   ; Appel du vecteur MODE
-	LD    H,20    ; H contient la colonne
-	LD    L,12    ; L contient la ligne
-	CALL  #BB75   ; Appel du vecteur LOCATE
-	LD    A,88    ; A chargé avec X
-	CALL  #BB5A   ; Appel du vecteur PRINT
-	call #bb06
-	ret
+	ORG &5000
+	LD HL,TEXT
+	CALL LOOP
+	CALL &BB06
+	RET
+LOOP    LD A,(HL)
+	CP 0
+	RET Z
+	CALL &BB5A
+	INC HL
+	JP LOOP
+;
+TEXT    DB "Hello!",0
